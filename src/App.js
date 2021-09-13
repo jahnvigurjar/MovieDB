8edf5b45bed288e990c75f303e13a24226e51ac5
Jahnvi Gurjar <jahnvigurjar27@gmail.com>
2021-09-13 17:05:35 +0530

    this is the code for moviedb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
+  
+  const featured_movies = process.env.REACT_APP_FEATURED_URL;
+  const search = process.env.REACT_APP_SEARCH_URL;
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+      getMovies(featured_movies)
+  }, []);
+
+  const getMovies = (api) => {
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+      setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+      getMovies(search + searchTerm);
+    }
+    setSearchTerm('');
+    }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+ 
+  const handleNavigation = (e) => {
+    e.preventDefault();
+    getMovies(featured_movies);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <header>
+      <button className="moviedb" onClick={handleNavigation}><h1>MOVIEDB</h1></button>
+      <form onSubmit={handleOnSubmit}>
+      <input className="search" type="search" placeholder="Search" value={searchTerm} onChange={handleOnChange} />
+      </form>
+    </header>
+
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((movie) => (
+        <Movie {...movie} key={movie.id} />
+      ))}
     </div>
+
+  </div>
   );
 }
 
